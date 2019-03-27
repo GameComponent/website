b9aea0ddd9e2113f60012012830ffb9355e4bd48
Niels <nielsla95@gmail.com>
2019-03-27 19:18:27 +0100

    fix(store): removed example store that broke generate command

--- store/index.ts
+++ store/index.ts
@@ -13,9 +13,10 @@ export const mutations: MutationTree<RootState> = {
 
 export const actions: ActionTree<RootState, RootState> = {
   async nuxtServerInit({ commit }, { app }) {
-    const people: Person[] = await app.$axios.$get(
-      "./random-data.json"
-    )
-    commit("setPeople", people.slice(0, 10))
+    // const people: Person[] = await app.$axios.$get(
+    //   "./random-data.json"
+    // )
+    // commit("setPeople", people.slice(0, 10))
+    commit("setPeople", [])
   }
 }
