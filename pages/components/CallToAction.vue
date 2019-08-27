61c7cee903c47a6b0b19f418742ae28b9fb17597
Niels <nielsla95@gmail.com>
2019-08-28 00:54:20 +0200

    improvement(CTA): updated CTA to signup

--- pages/components/CallToAction.vue
+++ pages/components/CallToAction.vue
@@ -1,31 +1,26 @@
 <template>
   <section class="py-16 md:py-32 md:flex md:justify-center md:items-center md:text-center">
     <div class="container mx-auto px-8">
-      <h2 class="text-black text-4xl" style="letter-spacing: 0.5px;">
-        Let's get started
-      </h2>
-      <p class="text-grey-darker mt-2 text-xl leading-normal" style="letter-spacing: 0.5px;">
-        Check out the source repository.
-      </p>
+      <h2 class="text-black text-4xl" style="letter-spacing: 0.5px;">Ready to get started?</h2>
+      <p
+        class="text-grey-darker mt-2 text-xl leading-normal"
+        style="letter-spacing: 0.5px;"
+      >Use our managed service or the self hosted economy service to get started.</p>
       <div class="mt-8">
         <div class="md:inline-block">
-          <a
-            href="https://github.com/GameComponent/economy-service"
+          <nuxt-link
+            to="/dashboard/register"
             class="inline-block py-4 px-6 rounded-sm bg-blue text-white no-underline font-medium text-sm hover:bg-blue-light"
             style="letter-spacing: 0.3px;"
-          >
-            Check out the service
-          </a>
+          >Sign up for free</nuxt-link>
         </div>
 
         <div class="mt-2 md:mt-0 md:ml-4 md:inline-block">
-          <a
-            href="https://github.com/GameComponent/economy-webclient"
+          <nuxt-link
+            to="/services/economy-service"
             class="inline-block py-4 px-6 rounded-sm bg-grey-darker text-white no-underline font-medium text-sm hover:bg-grey-dark"
             style="letter-spacing: 0.3px;"
-          >
-            Check out the webclient
-          </a>
+          >Check out the economy service</nuxt-link>
         </div>
       </div>
     </div>
@@ -35,10 +30,7 @@
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue
-} from "nuxt-property-decorator"
+import { Component, Vue } from "nuxt-property-decorator";
 
 @Component
 export default class CallToAction extends Vue {}
